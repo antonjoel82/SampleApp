9bf11577dd9fd5857ba9b62fea3c547ee1ae8115
Joel Anton <anton.joel82@gmail.com>
2019-05-12 03:22:36 +0000

    Transferring files from other project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import CoverPhoto from './Components/CoverPhoto/CoverPhoto';
+
+function App() {
+  return (
+    <CoverPhoto />
+  );
+}
+
+export default App;
---
+++ src/Components/CoverPhoto/CoverPhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CoverPhoto.css";
+
+const CoverPhoto = () => {
+	return (
+		<div className="cover-photo">
+			<h1>Cover Photo</h1>
+		</div>
+	);
+}
+export default CoverPhoto
